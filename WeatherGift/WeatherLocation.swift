b4715417d42e47b4d30367d5e425bea6b9ebcb3e
jgarcia956 <jordy808@gmail.com>
2022-02-09 22:34:46 -0600

    Ch 6

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Garcia, Jordan - Cathey on 2/7/22.
+//
+
+import Foundation
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longtitude: Double
+}
